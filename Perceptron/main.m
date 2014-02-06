594f044ce7fa29d45a930b5cf7b387a43cfbdd38
neonmori <neonmori@gmail.com>
2014-02-06 10:35:26 -0600

    Initial Commit

---
+++ Perceptron/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Perceptron
+//
+//  Created by Ke Yang on 2/6/14.
+//  Copyright (c) 2014 Pyrus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
